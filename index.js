30202a78be94a4ae3273462b667d3f366c4d80dd
ODyachenko <oleg1696@gmail.com>
2023-09-04 12:18:27 +0300

    JWT init

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 const app = express();
+app.use(express.json());
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello world');
+});
+
+app.post('/auth/login', (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    process.env.AUTH_KEY
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(4444, (err) => {
