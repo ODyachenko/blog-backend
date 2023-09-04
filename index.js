3fe02bd57166048c4eeea500ca7147bd468c3cf3
ODyachenko <oleg1696@gmail.com>
2023-09-04 15:48:01 +0300

    Refactored code

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import express from 'express';
-import { validationResult } from 'express-validator';
 import mongoose from 'mongoose';
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-import { regValidation } from './validations/registration.js';
-import UserModel from './models/User.js';
-import bcrypt from 'bcrypt';
 import checkAuth from './utils/checkAuth.js';
-import User from './models/User.js';
+import { regValidation } from './validations/registration.js';
+import { getMe, login, registration } from './components/UserController.js';
 
 const app = express();
 app.use(express.json());
@@ -20,122 +16,13 @@ mongoose
   .catch((err) => console.log('DB error', err));
 
 // User authorization
-app.post('/auth/login', async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
-
-    if (!user) {
-      return res.status(404).json({
-        message: 'User not found', //Краще не писати причину
-      });
-    }
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: 'Invalid login or password', //Краще не писати причину
-      });
-    }
-
-    // JWT token
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      process.env.JWT_KEY,
-      {
-        expiresIn: '30d',
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: 'Failed to log in',
-    });
-  }
-});
+app.post('/auth/login', login);
 
 // User Registration/Validation
-app.post('/auth/registration', regValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    // Encrypt the password
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    // Save user into DB
-    const user = await doc.save();
-
-    // JWT token
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      process.env.JWT_KEY,
-      {
-        expiresIn: '30d',
-      }
-    );
-
-    // Виключаємо пароль з відповіді
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: 'Registration failed',
-    });
-  }
-});
+app.post('/auth/registration', regValidation, registration);
 
 // Information about authorized user
-app.get('/auth/me', checkAuth, async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId);
-
-    if (!user) {
-      return res.status(404).json({
-        message: 'User not found',
-      });
-    }
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json(userData);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: 'Access denied',
-    });
-  }
-});
+app.get('/auth/me', checkAuth, getMe);
 
 app.listen(4444, (err) => {
   if (err) {
