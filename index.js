b2860c4b6004015d83421644a4ce91daf27b123e
ODyachenko <oleg1696@gmail.com>
2023-09-04 15:08:46 +0300

    User authorization

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import { validationResult } from 'express-validator';
 import mongoose from 'mongoose';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { regValidation } from './validations/registration.js';
-import { validationResult } from 'express-validator';
+import UserModel from './models/User.js';
+import bcrypt from 'bcrypt';
 
 const app = express();
 app.use(express.json());
@@ -15,16 +17,100 @@ mongoose
   .then(() => console.log('DB connected'))
   .catch((err) => console.log('DB error', err));
 
-// User Registration
-app.post('/auth/registration', regValidation, (req, res) => {
-  const errors = validationResult(req);
+// User authorization
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found', //Краще не писати причину
+      });
+    }
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Invalid login or password', //Краще не писати причину
+      });
+    }
+
+    // JWT token
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Failed to log in',
+    });
   }
-  res.json({
-    success: true,
-  });
+});
+
+// User Registration/Validation
+app.post('/auth/registration', regValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    // Encrypt the password
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
+
+    // Save user into DB
+    const user = await doc.save();
+
+    // JWT token
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      process.env.JWT_KEY,
+      {
+        expiresIn: '30d',
+      }
+    );
+
+    // Виключаємо пароль з відповіді
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: 'Registration failed',
+    });
+  }
 });
 
 app.listen(4444, (err) => {
